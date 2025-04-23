7a5115223facbc0114cdca1625b8c3c7da7c2f26
KCAmine <mejdiamine0544@gmail.com>
2025-04-23 02:15:23 +0200

    minor changes

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,56 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import { CgWebsite } from "react-icons/cg";
-import {  BsGithub } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
+  const [show, setShow] = useState(false);
+
   return (
-    <Card className="project-card-view">
+    <>
+      <Card className="project-card-view">
         <Card.Title>{props.title}</Card.Title>
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        
-        <Card.Text style= {{textAlign : "justify"}}>
-          <ul> 
-            <li><strong>Technologies : {props.technologies}</strong></li>
-          </ul>
-        </Card.Text>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
+        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+        <Card.Body>
+          <Card.Text style={{ textAlign: "justify" }}>
+            <ul>
+              <li><strong>Technologies : {props.technologies}</strong></li>
+            </ul>
+          </Card.Text>
 
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "Framagit"}
-        </Button>
-        {"\n"}
-        {"\n"}
+          <Card.Text style={{ textAlign: "justify" }}>
+            {props.description}
+          </Card.Text>
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+          <Button variant="primary" href={props.ghLink} target="_blank">
+            <BsGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "Framagit"}
           </Button>
-        )}
-      </Card.Body>
-    </Card>
+
+          {!props.isBlog && props.demoLink && (
+            <Button
+              variant="primary"
+              href={props.demoLink}
+              target="_blank"
+              style={{ marginLeft: "10px" }}
+            >
+              <CgWebsite /> &nbsp;
+              Demo
+            </Button>
+          )}
+
+          {/* Détails via modal */}
+          {props.hasModal && (
+            <Button
+              variant="secondary"
+              onClick={() => setShow(true)}
+              style={{ marginLeft: "10px" }}
+            >
+              Détails
+            </Button>
+          )}
+        </Card.Body>
+      </Card>
+
+      {/* Modal détail */}
+      {props.hasModal && (
+        <Modal
+          show={show}
+          onHide={() => setShow(false)}
+          size="lg"
+          centered
+          contentClassName="custom-modal-dark"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>{props.title} - Détails</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            {props.description2 && (
+              <div style={{ marginTop: "10px" }}>{props.description2}</div>
+            )}
+
+            {props.imgGallery && props.imgGallery.map((img, index) => (
+              <img
+                key={index}
+                src={img}
+                alt={`detail-${index}`}
+                style={{ width: "100%", marginTop: "20px", borderRadius: "8px" }}
+              />
+            ))}
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShow(false)}>
+              Fermer
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </>
   );
 }
+
 export default ProjectCards;
-
-
-
-
-
-
-
-
-
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,17 @@ import Particle from "../Particle";
 import analyser from "../../Assets/Projects/srcAnalyser.png";
 import scrappeur from "../../Assets/Projects/scrappeur.png"
 import boardgame from "../../Assets/Projects/boardgame.png";
+import Famille from "../../Assets/Projects/Acceuil_7Familles.png"
+import Arabe from "../../Assets/Projects/AL-KHWARIZMI_7Familles.png"
+import Trello from "../../Assets/Projects/TrelloPenate.png"
+import Charge from "../../Assets/Projects/CahierDEsCharges.png"
+import dev1 from "../../Assets/Projects/WorkflowDev1.png"
+import dev2 from "../../Assets/Projects/WorkflowDev2.png"
+import KanBan from "../../Assets/Projects/KanbanMiniStage.png"
+import Branche from "../../Assets/Projects/BrancheMiniStage.png"
+import Task from "../../Assets/Projects/TaskMiniStage.png"
+import env from "../../Assets/Projects/VarEnvMiniStage.png"
+import MiniStagePic from "../../Assets/Projects/MiniStagePic.png"
 
 
 
@@ -35,24 +46,95 @@ function Projects() {
           <Col md={4} className="project-card"  style={{ justifyContent: "center", paddingBottom: "15px", paddingTop: "30px"}}>
             <ProjectCards
               imgPath={boardgame}
-              isBlog={false}
+              hasModal={true}
               title="Maison du Jeu"
               technologies="PHP8 , PostGresSQL, JavaScript, HTML, CSS "
+              imgGallery={[Trello, Charge, dev1, dev2 ]}
               description="Un projet web complet où l’on peut voir chaque jeu de société loué, avec une représentation visuelle du jeu, ses règles, et quand il est disponible à la location."
               framagitLink="https://framagit.org/Hugo_wz/maisondujeu"
+              description2={
+                <>
+                  <p>
+                    Projet réalisé en groupe pour refondre le site de l’association Maison du Jeu.
+                    Nouvelle version dynamique avec gestion des utilisateurs et des réservations.
+                  </p>
+                  <ul>
+                    <li>📚 Base de données PostgreSQL pour gérer les jeux</li>
+                    <li>🔐 Espace public + intranet avec authentification</li>
+                    <li>💻 Backend en PHP pour traitement côté serveur</li>
+                    <li>🛠️ Travail en mode projet avec Git (Framagit)</li>
+                  </ul>
+                </>
+              }
             />
           </Col>
 
-          <Col md={4} className="project-card">
+          <Col md={4} className="project-card"  style={{ justifyContent: "center", paddingBottom: "15px", paddingTop: "30px"}}>
             <ProjectCards
-              imgPath={scrappeur}
-              isBlog={false}
-              title="Scrappeur "
-              technologies="JavaScript, HTML"
-              description="Scrappeur web réalisé pour un particulier. Il gère la pagination, récupère les données sur chaque page, puis génère un fichier CSV contenant toutes les informations collectées."
-              ghLink="https://github.com/KCAmine/JavaScript"
+              imgPath={Famille}
+              hasModal={true}
+              title="7 Famille"
+              technologies="HTML, CSS "
+              description="Site web statique basé sur le concept du jeu des 7 familles, adapté au domaine de l’informatique."
+              framagitLink="https://framagit.org/Hugo_wz/maisondujeu"
+              imgGallery={[Arabe]}
+              description2={
+                <>
+                  <p>
+                    Projet de groupe réalisé en HTML/CSS autour du jeu des 7 familles version informatique.
+                    Chaque élève présentait un personnage historique lié à une thématique (pionniers, matériel...).
+                  </p>
+                  <ul>
+                    <li>Site statique éco-conçu et responsive</li>
+                    <li>Travail en groupe avec répartition des pages</li>
+                    <li>Notions de SEO, accessibilité et écoconception appliquées</li>
+                  </ul>
+                </>
+              }
             />
           </Col>
+
+          <Col md={7} className="project-card"  style={{ justifyContent: "center", paddingBottom: "15px", paddingTop: "30px"}}>
+          <ProjectCards
+          title="Ministages44"
+          imgPath={MiniStagePic}
+          technologies="Symfony, PHP, MySQL, JavaScript, Git"
+          description="Plateforme de réservation de mini-stages pour les élèves, refaite en mode projet avec Symfony et sécurisation complète du site."
+          ghLink="https://framagit.org/TON_PROJET"
+          hasModal={true}
+          imgGallery={[Task, env, KanBan, Branche]} // ← si tu veux afficher des images dans le modal
+          description2={
+            <>
+              <p>
+                Projet en équipe pour refondre un site de gestion de mini-stages : corrections, sécurité, nouvelle version Symfony.
+              </p>
+
+              <h5>🚀 Missions réalisées</h5>
+              <ul>
+                <li>Corrections de bugs + création d’une nouvelle base</li>
+                <li>Développement Symfony 7 (backend + routes sécurisées)</li>
+                <li>Réorganisation complète du projet en 4 équipes (admin, réservation...)</li>
+              </ul>
+
+              <h5>🔐 Sécurité</h5>
+              <ul>
+                <li>Controle d'accès pour chaque rôle utilisateurs </li>
+                <li>Utilisation de requêtes préparées (anti-SQLi)</li>
+                <li>Mise en place d'un fichier de configuration de variables d'environnement</li>
+                <li>Validation et mise à jour des <code>expression régulières </code></li>
+              </ul>
+
+              <h5>📁 Travail collaboratif</h5>
+              <ul>
+                <li>Suivi par tickets / branches sur Framagit</li>
+                <li>Scrum masters désignés chaque semaine</li>
+                
+              </ul>
+            </>
+          }
+        />
+        /</Col>
+
         </Row>
       </Container>
     </Container>
